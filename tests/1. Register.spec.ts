823c1645a8ba155bbdcb5ba256d639e381bbf5a5
Bhaskar Majumdar <66664289+Bhaskar-Majumdar@users.noreply.github.com>
2026-03-20 00:22:13 +0600

    update registration file

--- tests/1. Register.spec.ts
+++ tests/1. Register.spec.ts
@@ -7,37 +7,36 @@ import { readLatestRegistrationEmail } from "../services/email.service.ts";
 import { content } from 'googleapis/build/src/apis/content/index.ts';
 
 test("Register as a new user", async ({ page }) => {
+  await page.goto("https://dailyfinance.roadtocareer.net/");
+  await page.getByRole("link",{name:"Register"}).click();
 
-    await page.goto("https://dailyfinance.roadtocareer.net/");
-    await page.getByRole("link",{name:"Register"}).click();
+  const register = new RegisterPage(page);
 
-    const register = new RegisterPage(page);
-
-    const userModel:UserModel = {
-        firstName: faker.person.firstName(),
-        lastName: faker.person.lastName(),
-        email:`majumdar97b+${generateRandomNumber(1000,9999)}@gmail.com`,
-        password:"1234",
-        phoneNumber:`01${generateRandomNumber(300000000,999999999)}`,
-        address:faker.location.streetAddress()
+  const userModel:UserModel = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email:`majumdar97b+${generateRandomNumber(1000,9999)}@gmail.com`,
+    password:"1234",
+    phoneNumber:`01${generateRandomNumber(300000000,999999999)}`,
+    address:faker.location.streetAddress()
     }
 
-    await register.userRegister(userModel);
+  await register.userRegister(userModel);
 
-    saveJsonData(userModel,'./resources/users.json');
+  saveJsonData(userModel,'./resources/users.json');
 
-    let latestEmail = await readLatestRegistrationEmail();
+  let latestEmail = await readLatestRegistrationEmail();
 
-    if (!latestEmail) {
-      throw new Error("No email received!");
-    }
+  if (!latestEmail) {
+    throw new Error("No email received!");
+  }
 
-    // latestEmail will be non-null
-    latestEmail = latestEmail.replace("&#39;", "'");
+  // latestEmail will be non-null
+  latestEmail = latestEmail.replace("&#39;", "'");
 
-    await page.waitForTimeout(6000);
+  await page.waitForTimeout(6000);
     
-    expect(latestEmail).toContain("Welcome to our platform");
+  expect(latestEmail).toContain("Welcome to our platform");
 
 
 });
